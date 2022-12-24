6fcc7f3e5d2251c0bc894a7a038e5e666860d52e
MANISH PRABHAKAR <manishprabhakar@outlook.com>
2022-12-24 16:57:28 +0530

    Initial commit

---
+++ app/src/main/java/com/example/glidelibrary/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.glidelibrary;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        @SuppressLint({"MissingInflatedId", "LocalSuppress"}) ImageView image1=(ImageView) findViewById(R.id.image1);
+        @SuppressLint({"MissingInflatedId", "LocalSuppress"}) ImageView image2=(ImageView) findViewById(R.id.image2);
+
+
+        Glide.with(this).load("https://play-lh.googleusercontent.com/ZyWNGIfzUyoajtFcD7NhMksHEZh37f-MkHVGr5Yfefa-IX7yj9SMfI82Z7a2wpdKCA=w240-h480-rw").into(image1);
+        Glide.with(this).load("https://play-lh.googleusercontent.com/ZyWNGIfzUyoajtFcD7NhMksHEZh37f-MkHVGr5Yfefa-IX7yj9SMfI82Z7a2wpdKCA=w240-h480-rw").into(image2);
+    }
+}
